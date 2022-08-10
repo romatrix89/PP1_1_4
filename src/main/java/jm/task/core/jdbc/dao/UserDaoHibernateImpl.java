178d20376f2fa5ca4361966c3c634629e41ebf71
romatrix89 <rtsagolov@gmail.com>
2022-08-10 17:00:25 +0300

    Error correction v3

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -3,9 +3,12 @@ package jm.task.core.jdbc.dao;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
+
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import java.util.List;
+import java.util.Objects;
 
 public class UserDaoHibernateImpl implements UserDao {
     private static final String SQL_CREATE_TABLE = "CREATE TABLE IF NOT EXISTS users " +
@@ -15,80 +18,65 @@ public class UserDaoHibernateImpl implements UserDao {
     private static final String SQL_DELETE_TABLE = "DROP TABLE IF EXISTS users";
 
     public UserDaoHibernateImpl() {
-
     }
 
     @Override
     public void createUsersTable() {
-        Session session;
-        try {
-            session = Util.getSessionFactory().openSession();
-            session.beginTransaction();
+        Transaction transaction = null;
+        try (Session session = Util.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
             session.createNativeQuery(SQL_CREATE_TABLE, User.class).executeUpdate();
-            session.getTransaction().commit();
+            transaction.commit();
         } catch (RuntimeException e) {
-            Util.getSessionFactory().getCurrentSession().getTransaction().rollback();
-        } finally {
-            Util.getSessionFactory().getCurrentSession().close();
+            Objects.requireNonNull(transaction).rollback();
         }
     }
 
     @Override
     public void dropUsersTable() {
-       Session session;
-        try {
-            session = Util.getSessionFactory().openSession();
-            session.beginTransaction();
+        Transaction transaction = null;
+        try (Session session = Util.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
             session.createNativeQuery(SQL_DELETE_TABLE, User.class).executeUpdate();
-            session.getTransaction().commit();
+            transaction.commit();
         } catch (RuntimeException e) {
-            Util.getSessionFactory().getCurrentSession().getTransaction().rollback();
-        } finally {
-            Util.getSessionFactory().getCurrentSession().close();
+            Objects.requireNonNull(transaction).rollback();
         }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-        Session session;
-        try {
-            session = Util.getSessionFactory().openSession();
-            session.beginTransaction();
+        Transaction transaction = null;
+        try (Session session = Util.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
             User newUser = new User();
             newUser.setName(name);
             newUser.setLastName(lastName);
             newUser.setAge(age);
             session.save(newUser);
-            session.getTransaction().commit();
+            transaction.commit();
         } catch (RuntimeException e) {
-            Util.getSessionFactory().getCurrentSession().getTransaction().rollback();
-        } finally {
-            Util.getSessionFactory().getCurrentSession().close();
+            Objects.requireNonNull(transaction).rollback();
         }
     }
 
     @Override
     public void removeUserById(long id) {
-        Session session;
-        try {
-            session = Util.getSessionFactory().openSession();
-            session.beginTransaction();
+        Transaction transaction = null;
+        try (Session session = Util.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
             User delUser = session.get(User.class, id);
             session.delete(delUser);
-            session.getTransaction().commit();
+            transaction.commit();
         } catch (RuntimeException e) {
-            Util.getSessionFactory().getCurrentSession().getTransaction().rollback();
-        } finally {
-            Util.getSessionFactory().getCurrentSession().close();
+            Objects.requireNonNull(transaction).rollback();
         }
     }
 
     @Override
     public List<User> getAllUsers() {
         List<User> entityList;
-        Session session;
-        try {
-            session = Util.getSessionFactory().openSession();
+        try (Session session = Util.getSessionFactory().openSession()) {
             CriteriaBuilder builder = session.getCriteriaBuilder();
             CriteriaQuery<User> criteria = builder.createQuery(User.class);
             criteria.from(User.class);
@@ -96,26 +84,21 @@ public class UserDaoHibernateImpl implements UserDao {
             for (User e : entityList) {
                 System.out.println(e.toString());
             }
-        } finally {
-            Util.getSessionFactory().getCurrentSession().close();
         }
         return entityList;
     }
 
     @Override
     public void cleanUsersTable() {
-        Session session;
-        try {
-            session = Util.getSessionFactory().openSession();
-            session.beginTransaction();
+        Transaction transaction = null;
+        try (Session session = Util.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
             for (User e : getAllUsers()) {
                 session.delete(e);
             }
-            session.getTransaction().commit();
+            transaction.commit();
         } catch (RuntimeException e) {
-            Util.getSessionFactory().getCurrentSession().getTransaction().rollback();
-        } finally {
-            Util.getSessionFactory().getCurrentSession().close();
+            Objects.requireNonNull(transaction).rollback();
         }
     }
 }
